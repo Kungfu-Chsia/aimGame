4b170271047eacf406c642310b25cb30b87aa05c
Kungfu-Chsia <allrighty@ya.ru>
2022-01-09 20:16:27 +0300

    random color

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const screens = document.querySelectorAll('.screen');
 const timeList = document.querySelector('#time-list');
 const timeEl = document.querySelector('#time');
 const board = document.querySelector('#board');
+const colors = ['#e74c3c', '#8e44ad', '3498db', '#e67e22', '#2ecc71'];
 
 let time = 0;
 let score = 0;
+
 
 startButton.addEventListener('click', (event) => {
   event.preventDefault()
@@ -69,10 +71,15 @@ function createRandomCircle () {
   circle.style.height = `${size}px`;
   circle.style.top = `${y}px`;
   circle.style.left = `${x}px`;
-
+  circle.style.background = getRandomColor();
+  
   board.append(circle);
 }
 
 function getRandomNumber (min, max) {
   return Math.round(Math.random() * (max - min) + min)
+}
+
+function getRandomColor() {
+  return colors[Math.floor(Math.random() * colors.length)];
 }
